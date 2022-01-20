546c238ad27a23816ec791e57b2a3360607a0e4a
caiocipriano <caio.silvax@hotmail.com>
2022-01-20 10:02:11 -0300

    20/01

--- src/br/com/fuctura/repository/ContaRepository.java
+++ src/br/com/fuctura/repository/ContaRepository.java
@@ -1,9 +1,13 @@
 package br.com.fuctura.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import br.com.fuctura.models.Conta;
+import br.com.fuctura.models.Usuario;
 
 public class ContaRepository {
 	public void incluir(Conta novaConta, EntityManager em) {
@@ -13,5 +17,12 @@ public class ContaRepository {
 		transacao.begin();
 		em.persist(novaConta);
 		transacao.commit();
+	}
+	
+	public List<Conta>pesquisarPorTodos(EntityManager em){
+		Query consulta = em.createQuery("SELECT c FROM Conta c");
+		List<Conta> resultado = consulta.getResultList();
+		return resultado;
+		
 	}
 }
